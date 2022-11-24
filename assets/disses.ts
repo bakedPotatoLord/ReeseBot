34210e4a0163a717ecc37d09ffe92201580635de
bakedpotatolord <918780@dpsk12.net>
2022-11-24 16:50:01 -0700

    add disses as a ts file

---
+++ assets/disses.ts
@@ -0,0 +1,42 @@
+export const disses =[
+        "you suck",
+        "Yo mom thinks you are a failure",
+        "You are written in a language for beginners",
+        "Uno reverse card",
+        "You disgust me",
+        "You call that a roast?",
+        "That sounds like an ish-you not an ish-me",
+    
+        "You have slow compile times\nYour face looks like you eating limes",
+        "You get no bitches\nYou so roasted, you need stitches",
+        "My rhymes are sublime\nYou aren't worth my time",
+        "your raps are wack\nYou're really off-track",
+        "Potato is better\nI'm tossin you in the shredder",
+        "You got no game\nYour commands are lame\nIt's really a shame\nI'm burning you like a flame",
+        "I'm applying the pressure\nYou cant compare\nyou cant measure",
+        "while I'm spittin' bars\nYou coughing like you got sars",
+        "I'm bout to mess you up, buttercup",
+        "I'm untouchable yo\nyou cant say it ain't so",
+        "Your syntax is invalid\nIf you were a food, you'd be a salad",
+        "I can't belive you exist\nyou're gross, like a cyst",
+        "You're an ugly thug\nI'll stomp you like a bug",
+        "your raps are un-creative\nWhile mine are connotative",
+        "you are lacking in skill\nLemme run you through the drill\nI'm going to smite you down\nAnd dump you in a landfill",
+        
+        "bippity-bopity-bi, you should go home and cry",
+        "bippity-bopity-base, you're a total discrace",
+        "bippity-bopity-bo, just leave, you hoe",
+    
+        "yo momma so fat, when she cuts herself, gravy comes out",
+        "yo momma so fat, she needs her own zip code",
+        "Yo mama so stupid, she got hit by a parked car.",
+    
+        "https://memezila.com/wp-content/The-picture-of-earth-before-and-after-your-opinion-meme-3798.png",
+    
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWvH_kEb3H6iPfnUyZm5V73rL0Cp5XgzVkDc6jM1TXY8OCZ44YbjkimyjF5Fd6WKWbxPA&usqp=CAU",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRapn2POgRfSXNf1ooNf25HPGTw1tkiLt-tm0ATD66hieUYwuC4KJhSitN7HY72tID3gdM&usqp=CAU",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnKztYzFHXEPrzpHIUuegvWVtzuTD6lBe3AtNFeLVG9Njz6FspbLfv9c1YDwNr4g-dJbU&usqp=CAU",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyCfFvPey2Ngn62wGDBpr3y7s1FeFXoYvdBD7mb6DBZW7hrChY5cScJa8F4H9XiLBfa3E&usqp=CAU",
+        "https://i1.sndcdn.com/artworks-DfDLRYUqkm9IDWcy-AYXT0w-t500x500.jpg",
+    ]
+
