85e80b594eec496ee0b120b152c9fbb9d3a1111c
bakedpotatolord <918780@dpsk12.net>
2022-11-24 18:25:44 -0700

    created stable base

--- index.ts
+++ index.ts
@@ -1,9 +1,8 @@
 
-import fs  from 'node:fs'
+import fs  from 'node:fs/promises'
 import path from 'node:path';
-import { Client, Collection, IntentsBitField, ActivityType } from 'discord.js';
+import { Client, Collection, IntentsBitField, ActivityType, Events } from 'discord.js';
 import { disses, TOKEN } from './consts.js';
-
 import { fileURLToPath } from 'url'
 import { dirname } from 'path'
 const __filename = fileURLToPath(import.meta.url)
@@ -16,10 +15,24 @@ const client: Client = new Client({ intents: [
 	IntentsBitField.Flags.GuildEmojisAndStickers
 ] });
 
-client.commands = new Collection();
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
+client.commands = new Collection();
+const commandFiles = (await fs.readdir("./build/commands")).filter(file => file.endsWith('.js'));
+
+// Grab all the command files from the commands directory you created earlier
+console.log(`found ${commandFiles.length} command files`)
+
+
+for (const file of commandFiles) {
+
+	const command = (await import(`./commands/${file}`)).default;
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command is missing a required "data" or "execute" property.`);
+	}
+}
 
 client.once('ready', () => {
 	//registerSelf(client)
@@ -31,16 +44,12 @@ client.once('ready', () => {
 	if(client.readyTimestamp){
     console.log(`Ready at ${new Date(client.readyTimestamp)}`);
   }
-	
 });
 
-client.on('interactionCreate', async interaction => {
+client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isCommand()) return;
-
-	let command = client.commands.get(interaction.commandName);
-
+	const command = client.commands.get(interaction.commandName);
 	if (!command) return;
-
 	try {
 		await command.execute(interaction);
 	} catch (error) {
@@ -49,23 +58,19 @@ client.on('interactionCreate', async interaction => {
 	}
 });
 
-client.on('messageCreate', message => {
+client.on(Events.MessageCreate, message => {
 	console.log("recived a message")
 
 	if(message.author.id == "790765656571379762"){
-
 		message.react('😡')
 		message.reply(disses[Math.floor(Math.random()*disses.length)])
 	}
-
 	if(checkforWord(message.content,"balls")){
 		message.react('🤨')
 	}else if(checkforWord(message.content,"amongus") || checkforWord(message.content,"amogus")){
 		message.react('🤢')
 	}
-
 });
-
 
 client.login(TOKEN);
 
