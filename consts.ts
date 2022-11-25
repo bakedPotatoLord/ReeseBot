9e617099a9524441b9363fef32b0691aba70fbb3
bakedpotatolord <918780@dpsk12.net>
2022-11-24 17:42:02 -0700

    get consts more efficiently

--- consts.ts
+++ consts.ts
@@ -1,22 +1,14 @@
 import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config()
-
 export {disses} from "./assets/disses.js"  ; 
 
-function getToken():string{
-  if(process.env.TOKEN){
-    return process.env.TOKEN
-  }else{
-    throw new Error("no token in the env file")
-  }
-}
-const TOKEN =getToken()
+export const TOKEN = process.env["TOKEN"]
+export const CLIENTID = process.env["CLIENTID"]
 
-export const clientIds:string[] = [
+export const guildIds:string[] = [
   "986808066353815595"
 ]
 
-export {TOKEN}
 
 
 
