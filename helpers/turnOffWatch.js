1c5718d7f615a1904e9976dc6bbe9c9a65418c39
bakedpotatolord <918780@dpsk12.net>
2022-11-24 20:26:03 -0700

    add removewatch script

---
+++ helpers/turnOffWatch.js
@@ -0,0 +1,11 @@
+import fs from "node:fs/promises"
+
+
+let data = JSON.parse(await fs.readFile("./tsconfig.json","utf-8"))
+
+
+data.compilerOptions.watch = false
+
+console.log(data)
+
+await fs.writeFile("./tsconfig.json",JSON.stringify(data))
