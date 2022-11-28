0158d835c3af7d7d84beed8a36ea605d705072d7
bakedpotatolord <918780@dpsk12.net>
2022-11-28 11:00:44 -0700

    change to ts and change command location

--- helpers/turnOffWatch.ts
+++ helpers/turnOffWatch.ts
@@ -1,11 +1,7 @@
 import fs from "node:fs/promises"
 
-
 let data = JSON.parse(await fs.readFile("./tsconfig.json","utf-8"))
-
-
 data.compilerOptions.watch = false
-
 console.log(data)
 
 await fs.writeFile("./tsconfig.json",JSON.stringify(data))
